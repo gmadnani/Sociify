f0949c255c0d46f09919c9d0969a16aa01d65e01
Zain Abidin <xan.abdn@gmail.com>
2022-04-09 11:18:47 +0100

    wired up private route to index

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
+const privateRoutes = require("./privateRoutes")
 
 router.use("/", homeRoutes);
+router.use("/profile",privateRoutes)
 router.use("/api", apiRoutes);
 
 module.exports = router;
