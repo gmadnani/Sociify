409b8bf0c91c8befecd578a3883b89be7fabd881
Girish Madnani <girishmadnani05@gmail.com>
2022-04-13 14:35:38 +0100

    model profile

---
+++ models/profile.js
@@ -0,0 +1,84 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Profile extends Model {}
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    number: {
+        type: DataTypes.NUMBER,
+        allowNull: true,
+    },
+    city: {
+         type: DataTypes.STRING,
+         allowNull: true,
+    }, 
+    country: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    photo: {
+        type: DataTypes.STRING, 
+        allowNull: true,
+    },
+    skills: {
+        type: DataTypes.ARRAY,
+        allowNull: true,
+    },
+    aboutme: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    experience: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    hobbies: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    facebook_link: {
+        type: DataTypes.STRING,
+        allowNull: true,
+
+    },
+    linkedin_link: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    github_link: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    privacy: {
+        type: DataTypes.STRING,
+        allowNull: true,
+
+    },
+    userID: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'User',
+            key: 'id',
+        },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "profile",
+  }
+);
+
+module.exports = Profile;
