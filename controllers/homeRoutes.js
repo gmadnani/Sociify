ab929a1464d373bf5ff446d8cfe0c772f2a3978e
Girish Madnani <girishmadnani05@gmail.com>
2022-04-15 17:45:43 +0100

    fix conflicts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,32 +13,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
-=======
-// Use withAuth middleware to prevent access to route
-router.get("/profile", withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: Profile }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    console.log("logged in successfully");
-
-    // Open edit profile page after login
-    res.render("edit-profile", {
-      ...user,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
->>>>>>> main
 // If the user is already logged in, redirect to profile
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
@@ -60,11 +34,7 @@ router.get("/register", (req, res) => {
 });
 
 // Get all profiles page
-<<<<<<< HEAD
 router.get("/allProfiles", withAuth , async (req, res) => {
-=======
-router.get("/all", withAuth, async (req, res) => {
->>>>>>> main
   try {
     // Get all profiles and JOIN with user data
     const profileData = await Profile.findAll({
@@ -88,7 +58,6 @@ router.get("/all", withAuth, async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
 router.get("/myProfile", withAuth, async (req, res) => {
   try{
     const userData = await User.findByPk(req.session.user_id, {
@@ -130,12 +99,6 @@ router.get("/editProfile", withAuth, async (req, res) => {
 });
 
 router.get("/singleprofile", withAuth, async(req, res) => {
-=======
-router.get("/myprofile/id", withAuth, async (req, res) => {
-  res.render("my-profile");
-});
-router.get("/singleprofile", withAuth, async (req, res) => {
->>>>>>> main
   res.render("single-profile");
 });
 
