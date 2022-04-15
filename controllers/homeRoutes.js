008b00d177803aa63456b8e397da8008d3d6d230
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-04-15 17:29:02 +0100

    route for single profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -81,11 +81,51 @@ router.get("/all", withAuth, async (req, res) => {
   }
 });
 
-router.get("/myprofile/id", withAuth, async (req, res) => {
-  res.render("my-profile");
+// Get single profile page by id
+router.get("/profiles/:id", withAuth, async (req, res) => {
+  try {
+    // Get profiles and JOIN with user data
+    const profileData = await Profile.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const profile = profileData.get({ plain: true });
+
+    // console.log("--------------------------------------------");
+    console.log(profile);
+    // Pass serialized data and session flag into template
+    res.render("single-profile", {
+      profile,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-router.get("/singleprofile", withAuth, async (req, res) => {
+
+//
+// router.get("/:id", async (req, res) => {
+//   const profileData = await Profile.findOne({
+//     where: { id: req.params.id },
+//   }).catch((err) => {
+//     res.json(err);
+//   });
+//   res.json(profileData);
+// });
+
+router.get("/profile/:id", withAuth, async (req, res) => {
   res.render("single-profile");
+});
+
+router.get("/myprofile/:id", withAuth, async (req, res) => {
+  res.render("my-profile");
 });
 
 module.exports = router;
