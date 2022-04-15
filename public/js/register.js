21d8578488277885eb91032abef39b97c21e4f75
Girish Madnani <girishmadnani05@gmail.com>
2022-04-15 18:53:41 +0100

    register linking

--- public/js/register.js
+++ public/js/register.js
@@ -1,33 +1,35 @@
-
-
-
-// const registerFormHandler = async (event) => {
-//     event.preventDefault();
+const registerFormHandler = async (event) => {
+    event.preventDefault();
   
-//     // Collect values from the login form
-//     const email = document.querySelector('#RegisterEmail').value.trim();
-//     const password = document.querySelector('#RegisterPassword').value.trim();
-//     console.log(email,password)
+    // Collect values from the login form
+    const email = document.querySelector('#RegisterEmail').value.trim();
+    const password = document.querySelector('#RegisterPassword').value.trim();
+    const name = document.querySelector('#RegisterName').value.trim();
+    console.log(email, password, name)
   
-//     if (email && password) {
-//       // Send a POST request to the API endpoint
-//       const response = await fetch('/api/users/', {
-//         method: 'POST',
-//         body: JSON.stringify({ email, password }),
-//         headers: { 'Content-Type': 'application/json' },
-//       });
+    if (email && password && name) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/register', {
+        method: 'POST',
+        body: JSON.stringify({ email, password, name }),
+        headers: { 'Content-Type': 'application/json' },
+      });
   
-//       if (response.ok) {
-//           console.log("we are logged in")
-//         // If successful, redirect the browser to the profile page
-//         document.location.replace('/api/profile');
-//       } else {
-//         alert(response.statusText);
-//       }
-//     }
-//   };
+      if (response.ok) {
+          console.log("we are logged in")
+
+          const newProfile = await fetch('/api/profile/createProfile', {
+              method: 'POST'
+          });
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/editProfile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
   
 
-// document
-//   .querySelector('.registerForm')
-//   .addEventListener('submit', registerFormHandler);
+document
+  .querySelector('.registerForm')
+  .addEventListener('submit', registerFormHandler);
